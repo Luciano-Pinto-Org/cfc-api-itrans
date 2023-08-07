6068be22f9dc81bc759682dc3445a203f7d45810
Luciano da Costa Pinto <luciano.pinto@gmail.com>
2023-08-07 14:56:24 -0400

    Inclusão dos endpoints alunos e instrutores

--- src/main/java/br/gov/am/prodam/cfc_api_itrans/model/dto/ImagensDTO.java
+++ src/main/java/br/gov/am/prodam/cfc_api_itrans/model/dto/ImagensDTO.java
@@ -3,12 +3,14 @@ package br.gov.am.prodam.cfc_api_itrans.model.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
 @Setter
 public class ImagensDTO implements Serializable{
 
+	@Serial
 	private static final long serialVersionUID = 1L;
 	
 	public ImagensDTO() {}
